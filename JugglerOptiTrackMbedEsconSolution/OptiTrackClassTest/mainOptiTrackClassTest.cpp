6fda1f6a89aa3c163d4e34defc1b167fd1277704
Robert Katzschmann <rkk@csail.mit.edu>
2017-01-17 15:09:24 -0500

    optitrack class compiling

---
+++ JugglerOptiTrackMbedEsconSolution/OptiTrackClassTest/mainOptiTrackClassTest.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <iostream>
+#include <cstdio>
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
+
+//#include <windows.h>
+//#include <math.h>
+#include "OptiTrack.h"
+
+
+int main()
+{	
+	OptiTrack optiTrack;
+	
+	int errorCode = optiTrack.initialize();
+	if (errorCode != ErrorCode_OK)
+	{
+		printf("Something went wrong initializing OptiTrack.");
+		return 1;
+	}
+
+	errorCode = optiTrack.enterMenuMode();
+		if (errorCode != ErrorCode_OK)
+		{
+			printf("Something went wrong in menu mode of OptiTrack.");
+			return 1;
+		}
+	return ErrorCode_OK;
+}
